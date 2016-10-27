b2a0e756f28525db92f60ee9e111c3a6771abff8
Stephen Anderson <sa3196a@student.american.edu>
2016-10-26 18:21:52 -0700

    Stephen Anderson did some debugging

--- app.js
+++ app.js
@@ -12,9 +12,9 @@ function renderHTMLHeading() {
 };
 renderHTMLHeading();
 
-function renderHTMLTable() {
-  console.log('renderHTMLTable');
-  var table = document.createElement('table');
+function setUpTable() {
+  console.log('setUpTable');
+  var table = document.getElementById('table');
   var main = document.getElementsByTagName('main')[0];
   main.appendChild(table);
 
@@ -42,8 +42,8 @@ function renderHTMLTable() {
   var table = document.getElementsByTagName('table')[0];
   table.appendChild(tbody);
 };
-renderHTMLTable();
-console.log('hello world');
+
+
 
 var CookieStore = function(name, minCust, maxCust, avgCookies) {
   this.name = name;
@@ -88,25 +88,42 @@ var CookieStore = function(name, minCust, maxCust, avgCookies) {
   storesArray.push(this);
 };
 
-var pike = new CookieStore ('First and Pike', 23, 65, 6.3);
-var airport = new CookieStore ('SeaTac Airport', 3, 24, 1.2);
-var center = new CookieStore ('Seattle Center', 11, 38, 3.7);
-var capitol = new CookieStore ('Capitol Hill', 20, 38, 2.3);
-var alki = new CookieStore ('Alki', 2, 16, 4.6);
+var pike = new CookieStore('First and Pike', 23, 65, 6.3);
+var airport = new CookieStore('SeaTac Airport', 3, 24, 1.2);
+var center = new CookieStore('Seattle Center', 11, 38, 3.7);
+var capitol = new CookieStore('Capitol Hill', 20, 38, 2.3);
+var alki = new CookieStore('Alki', 2, 16, 4.6);
 
-for (var i = 0; i < storesArray.length; i++) {
-  storesArray[i].renderHTMLSales();
+function fillInTable() {
+  var table = document.getElementById('table');
+  table.textContent = '';
+  setUpTable();
+  for (var i = 0; i < storesArray.length; i++) {
+    console.log(storesArray[i]);
+    storesArray[i].renderHTMLSales();
+  }
 }
-var newStore = [];
+fillInTable();
+
 var form = document.getElementById('form');
 
 function harvestForm(event) {
+  // var newStore = [];
   event.preventDefault();
-  for (i = 1; i < 6; i++) {
-    var formBox = event.target[i].value;
-    newStore.push(formBox);
-    console.log(formBox);
-  }
+  console.log(event);
+  var storeName = event.target.storeName.value;
+  console.log('storeName: ', storeName);
+  var minCust = event.target.minCust.value;
+  var maxCust = event.target.maxCust.value;
+  var avgCookies = event.target.avgCookies.value;
+
+  // for (var i = 1; i < 5; i++) {
+  //   var formBox = event.target[i].value;
+  //   newStore.push(formBox);
+  //   console.log(formBox);
+  // }
+  var newStore = new CookieStore(storeName, minCust, maxCust, avgCookies);
+
+  fillInTable();
 }
-form.addEventListener('submit', harvestForm, renderHTMLTable());
-storesArray.push(newStore);
+form.addEventListener('submit', harvestForm);
